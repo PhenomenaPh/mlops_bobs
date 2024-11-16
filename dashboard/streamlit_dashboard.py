738e122a6f46fc7fbfb63f644b867a17cc352b8f
pbrysh17 <pbrysh17@gmail.com>
2024-11-17 00:37:23 +0300

    Add Predict & Test Functionality

--- dashboard/streamlit_dashboard.py
+++ dashboard/streamlit_dashboard.py
@@ -65,6 +65,40 @@ def main():
             st.write("""**Now let's define the model...**""")
             db.create_and_train(features=features, targets=targets)
 
+    elif section == "Predict & Test":
+
+        st.subheader("Predict and Test")
+
+        try:
+            if len(selected_models) == 0:
+                models_check = False
+                st.warning("You have to train a model first.")
+            else:
+                models_check = True
+        except TypeError:
+            models_check = True
+        
+        if models_check:
+
+            st.write("""**First, let's download the predict/test set...**""")
+
+            predict_df = db.load_csv()
+
+            target = st.toggle("My data has Target", value=True)
+
+            features, targets, predict_test_check = db.check_predict_test_df(df=predict_df, target=target)
+
+            if predict_test_check:
+                
+                st.write("""**Now let's make some predictions...**""")
+                predictions = db.predict(selected_models=selected_models, features=features, df=predict_df)
+
+                st.write("""**Finally, let's test the quality of trained model(s)...**""")
+                if target:
+                    db.test(targets=targets, predictions=predictions)
+                else:
+                    st.warning("You have to have a Target to test your model(s).")
+
     elif section == 'Delete':
 
         st.subheader("Delete")
